5b7b4810195ed815d2b9da9dea64fc6fdec822bc
Abdessamadbda <abdessamadelbada02@gmail.com>
2024-04-26 00:08:31 +0100

    working on ui

--- src/components/cloudsafe/Navbar.js
+++ src/components/cloudsafe/Navbar.js
@@ -1,18 +1,24 @@
 import React from "react"
 import { Navbar, Nav } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function NavbarComponent() {
   return (
     <Navbar bg="light" expand="sm">
       <Navbar.Brand as={Link} to="/">
+        <img src="public/cloudsafe.png" alt=""/>
         CloudSafe
       </Navbar.Brand>
-      <Nav>
+      <Nav className="ml-auto"> {/* Align items to the right */}
         <Nav.Link as={Link} to="/user">
-          Profile
+        <FontAwesomeIcon
+      icon={faUser}
+      className="ml-4 mr-2 cursor-pointer"
+    />
         </Nav.Link>
       </Nav>
     </Navbar>
-  )
+  );
 }
